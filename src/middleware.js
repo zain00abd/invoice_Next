b1ceaa85de3edaa901423a8059b213bd7b920959
zain00abd <zainabflash1998@gmail.com>
2024-08-16 14:58:27 +0300

    ss

--- src/middleware.js
+++ src/middleware.js
@@ -1,9 +1,3 @@
-import { withAuth } from "next-auth/middleware"
+export { default } from "next-auth/middleware"
 
-export default withAuth({
-  // Matches the pages config in `[...nextauth]`
-  pages: {
-    signIn: "/signin",
-    error: "/error",
-  },
-})
+export const config = { matcher: ["/adduser"] }
