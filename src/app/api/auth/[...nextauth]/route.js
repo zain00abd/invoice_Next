1fbef6a0a343d4325eb287f9cf75df75860b1437
zain00abd <zainabflash1998@gmail.com>
2024-08-17 06:16:29 +0300

    ss

--- src/app/api/auth/[...nextauth]/route.js
+++ src/app/api/auth/[...nextauth]/route.js
@@ -34,7 +34,7 @@ export const authOptions = {
     }),
   ],
 
-  secret: process.env.NEXTAUTH_SECRET,
+  secret: "EXRUztpztZ4G8z/tacBW7nS4nAWpEHzYXyMFhtwYnZQ=",
 
   pages: {
     signIn: "/signin",
